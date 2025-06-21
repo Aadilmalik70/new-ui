5c9a787478ce11053190b451396c307099293479
Aadilmalik70 <76839054+Aadilmalik70@users.noreply.github.com>
2025-06-21 18:17:37 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -130,7 +130,7 @@ export default function SEODashboard() {
 
     setIsLoading(true)
     try {
-      const response = await fetch("http://144.24.104.158/api/process", {
+      const response = await fetch("https://app.serpstrategists.com/api/process", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
